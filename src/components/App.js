e581538f75d3847100608fea8e217ec5a8537d2d
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-05 13:31:34 -0800

    Build out component file structure, start form

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../css/App.css';
+import GameNightForm from './GameNightForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <GameNightForm />
+    </div>
+  );
+}
+
+export default App;
