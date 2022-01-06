a792241a89df441813b367e9beeb1e6a401e3c70
Adam Burgess <aburg1515@gmail.com>
2022-01-06 16:09:02 -0700

    Implement fetch call and map gameboard array

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -7,25 +7,34 @@ const GlobalProvider = (props) => {
 
   const fetchGames = async () => {
     try {
-      const gameData = await fetch('https://api.boardgameatlas.com/api/search?limit=10&client_id=DSHOCsJGeA')
+      const gameData = await fetch('https://api.boardgameatlas.com/api/search?order_by=rank&ascending=false&client_id=DSHOCsJGeA')
       const { games } = await gameData.json()
-      setGames(games)
-      console.log(games)
+      setGames(games.map(game => {
+        return {
+          id: game.id,
+          name: game.name,
+          price: game.price, 
+          min_players: game.min_players, 
+          max_players: game.max_players,
+          img: game.thumb_url
+        }
+      }).sort((a, b) => {
+        return a.name.localeCompare(b.name)
+      }))
     } catch (e) {
         console.log(e.message)
     }
   }
 
-useEffect(() => {
-  fetchGames()
-}, [])
+  useEffect(() => {
+    fetchGames()
+  }, [])
 
-return (
-  <GlobalContext.Provider value={games}>
-    {props.children}
-  </GlobalContext.Provider>
-)
-
+  return (
+      <GlobalContext.Provider value={games}>
+        {props.children}
+      </GlobalContext.Provider>
+    )
 }
 
 
