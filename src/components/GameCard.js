6ee7ad922762a686aef9e12747c2a89aa227d520
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-07 12:17:35 -0800

    Refactor played game & winner display to be game image

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalContext';
 import '../css/GameCard.css';
 
-const GameCard = ({ game, winner }) => {
+const GameCard = ({ gameName, winner }) => {
+  const { games } = useContext(GlobalContext)
+  const playedGame = games.find(game => game.name === gameName)
+
   return(
     <section className="game-card">
-      <p>Game: {game}</p>
+      <img src={playedGame.img} alt={`${gameName} game box`} />
       <p>Winner: {winner}</p>
     </section>
   )
--- src/components/GameResultsContainer.js
+++ src/components/GameResultsContainer.js
@@ -4,8 +4,7 @@ import GameCard from './GameCard'
 
 const GameResultsContainer = ({ gamesPlayed }) => {
   const displayGamesPlayed = gamesPlayed.map(game => {
-    console.log(game, "game")
-    return <GameCard game={game.game} winner={game.winner} />
+    return <GameCard gameName={game.game} winner={game.winner} />
   })
 
   return(
