32eab67ec8db0cb9b655e1124f85ad31ed256f9a
Adam Burgess <aburg1515@gmail.com>
2022-01-07 17:11:11 -0700

    Implement router

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../css/Nav.css';
+import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <header>
       <h1>Gamenight!</h1>
       <nav>
-        <li>About</li>
-        <li>Games</li>
+        <Link to={'/'}><li>Home</li></Link>
+        <Link to={'/about'}><li>About</li></Link>
+        <Link to={'/games'}><li>Games</li></Link>
       </nav>
     </header>
   )
