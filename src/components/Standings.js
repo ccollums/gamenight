d07f8925243b77d3f80e5db4d2ac54cf476947f2
Kyra Bergsund <k.bergsund@gmail.com>
2022-01-11 10:38:58 -0800

    Add content to standings display, polish styling

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -14,10 +14,21 @@ const Standings = () => {
       return obj
     }, {})
 
+  const generatePlaces = (index) => {
+    if (index === 0) {
+      return '1st'
+    } else if (index === 1) {
+      return '2nd'
+    } else {
+      return '3rd'
+    }
+  }
+
   const sortStandings = !winners.length ? <p>Currently no standings. Play some games!</p> : Object.entries(generateStandings).sort((a, b) => b[1] - a[1]).map((winner, index) => index <= 2 &&
     <div key={index}>
+      <p>{generatePlaces(index)}</p>
       <p className="winner">{winner[0]}</p>
-      <p className="wins">{winner[1]}</p>
+      <p className="wins">{winner[1] === 1 ? `${winner[1]} win` : `${winner[1]} wins`}</p>
     </div>
   )
 
